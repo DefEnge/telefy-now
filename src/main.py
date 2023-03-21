bc19c680d5ec42c631b376d430503d0bf6490670
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-03-21 08:40:44 +0100

    refactor: added a function that handles the bioi

--- src/main.py
+++ src/main.py
@@ -7,11 +7,14 @@ from pyrogram.raw import functions
 with open('../config/config.yaml' , 'r' , encoding='UTF-8') as file:
     data = yaml.load(file, Loader = SafeLoader)
 
+async def modify_bio(client, new_bio):
+    await client.invoke(query = functions.account.UpdateProfile(about= new_bio))
+
 
 async def main(api_id, api_hash, username, phone_number):
     """Main function"""
     async with Client(username, api_id=api_id, api_hash=api_hash, phone_number=phone_number) as app:
-        await app.invoke(query = functions.account.UpdateProfile(about="try"))
+        await modify_bio(app, "test")
 
 if __name__ == "__main__":
     asyncio.run(main(data['api_id'] , data['api_hash'] , data['username_account'], data['phone_number']))
