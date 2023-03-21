d74e87aa041fe9ea6c83ef02c48b108ec4487b43
Neos <cappellotancredi@gmail.com>
2023-03-21 20:04:38 +0100

    feat: interaction with the spotify api

---
+++ src/spotify/api.py
@@ -0,0 +1,22 @@
+from spotipy import Spotify, oauth2
+from typing import Union
+from . import track
+
+class SpotifyAPI(Spotify):
+    'Main class to interact with the Spotify Web API.'
+    def __init__(self, client_id: str, client_secret: str, redirect_uri: str = 'http://localhost:1024/redirect'):
+        Spotify.__init__(self, auth_manager = oauth2.SpotifyOAuth(
+            client_id = client_id,
+            client_secret = client_secret,
+            redirect_uri = redirect_uri,
+            scope = 'user-read-currently-playing',
+        ))
+    
+    def now_playing(self) -> Union[track.Track, None]:
+        result = self.current_user_playing_track()
+        return None if result is None else track.Track(result['item'])
+
+
+if __name__ == '__main__':
+    client = SpotifyAPI()
+    print(client.now_playing().title)
